459f87b69e59b4910b191a76a25bfa3284aaa42b
FalakD <30274347+FalakD@users.noreply.github.com>
2017-10-22 00:23:28 -0700

    saved page

--- MyApplication/app/src/main/java/com/dubhacks/maycontain/SavedActivity.java
+++ MyApplication/app/src/main/java/com/dubhacks/maycontain/SavedActivity.java
@@ -2,12 +2,23 @@ package com.dubhacks.maycontain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SavedActivity extends AppCompatActivity {
-
+    private Button addPlaceBtn;
+    private Dialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved);
+
+        addPlaceBtn = (Button) findViewById(R.id.addPlace);
+        addPlaceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog = new
+            }
+        });
     }
 }
