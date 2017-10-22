03522240237ef4f02114c5fe781a72ec7b50875e
FalakD <30274347+FalakD@users.noreply.github.com>
2017-10-21 23:11:04 -0700

    login/register firebase

---
+++ MyApplication/app/src/main/java/com/dubhacks/maycontain/User.java
@@ -0,0 +1,22 @@
+package com.dubhacks.maycontain;
+
+/**
+ * Created by Falak on 10/21/17.
+ */
+
+public class User {
+    private String email;
+    public User() {
+
+    }
+    public User(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+    public void setEmail(String s) {
+        this.email = s;
+    }
+}
